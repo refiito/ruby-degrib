52a3c3e870a0a88b3a061953d3d968d183910604
Margus <margus@apprise.eu>
2012-03-16 12:26:25 +0200

    initial

---
+++ lib/ruby-degrib.rb
@@ -0,0 +1,7 @@
+require "ruby-degrib/version"
+
+module Ruby
+  module Degrib
+    # Your code goes here...
+  end
+end
---
+++ lib/ruby-degrib/version.rb
@@ -0,0 +1,5 @@
+module Ruby
+  module Degrib
+    VERSION = "0.0.1"
+  end
+end
